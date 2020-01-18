547bbd223da98ee16c11f473e0e9c9323ec6797f
erdem <demirkapierdem@gmail.com>
2020-01-19 00:27:53 +0300

    change default for state

--- transactions/models.py
+++ transactions/models.py
@@ -7,8 +7,8 @@ class Transaction(models.Model):
     user = models.ForeignKey('users.CustomUser', related_name='user', on_delete = models.CASCADE)
     amount_cents = models.IntegerField(default = 0, null = False)
     amount_currency = models.CharField(default = 'TRY', max_length = 10, null = False)
-    state = models.CharField(max_length = 50, null = False)
+    state = models.CharField(max_length = 50, default = 'pending', editable = False)
     to_whom = models.ForeignKey('users.CustomUser', related_name='to_whom', on_delete = models.CASCADE)
-    
+
     def __str__(self):
         return self.note
