4ec114a467b2d60021c7ad1cca0d84543237e5cf
erdem <demirkapierdem@gmail.com>
2020-01-18 20:35:05 +0300

    send all data of foreign key objects

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -5,3 +5,4 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = ('id', 'created_date', 'updated_date', 'note', 'user', 'amount_cents', 'amount_currency', 'state', 'to_whom', )
+        depth = 1
