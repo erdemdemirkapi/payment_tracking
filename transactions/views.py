fc2bfdee2574a07a0d2a318155dcfcb6bdddd28b
erdem <demirkapierdem@gmail.com>
2020-01-18 21:40:13 +0300

    filter transactions

--- transactions/views.py
+++ transactions/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
 
 from . import models
 from . import serializers
+from django.core.serializers import serialize
 
 class TransactionView(generics.ListCreateAPIView):
     queryset = models.Transaction.objects.all()
     serializer_class = serializers.TransactionSerializer
+
+    def get(self, request, pk = None):
+        current_user = request.user
+        transactions = models.Transaction.objects.filter(user__id = current_user.id)
+        data = serialize('json', transactions)
+        response = {
+            'data': data
+        }
+        return Response(response)
