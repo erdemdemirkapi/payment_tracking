0160232e1392d14a073f2d0471d06b201d82d3d5
erdem <demirkapierdem@gmail.com>
2020-01-18 22:47:01 +0300

    bug fix foreign key

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source = 'user.username')
+    to_whom_username = serializers.ReadOnlyField(source = 'to_whom.username')
+
     class Meta:
         model = Transaction
-        fields = ('id', 'created_date', 'updated_date', 'note', 'user', 'amount_cents', 'amount_currency', 'state', 'to_whom', )
-        depth = 1
+        fields = ('id', 'created_date', 'updated_date', 'note', 'username', 'amount_cents', 'amount_currency', 'state', 'to_whom_username', )
