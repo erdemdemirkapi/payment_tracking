31ddfe5b0bdbba017f94cc4bb90c06b5c2748637
erdem <demirkapierdem@gmail.com>
2020-01-19 10:10:09 +0300

    add send mail url

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include, path
 
+from mails.views import Mails
+
 urlpatterns = [
     path('users/', include('users.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('transactions/', include('transactions.urls')),
+    path('transactions/', include('transactions.urls')),
+    path('send-mail/', Mails.as_view(), name='mails'
 ]
